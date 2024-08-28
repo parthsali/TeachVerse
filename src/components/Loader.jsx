fdb4b6f8cc650dc7c0d6af82d4a611ef44e84500
ParthSali <parthsali04@gmail.com>
2024-08-28 17:40:04 +0530

    Classroom done

---
+++ src/components/Loader.jsx
@@ -0,0 +1,17 @@
+const Loader = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+
+export default Loader;
--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,18 +1,42 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+  CameraControls,
+  Environment,
+  Gltf,
+  OrbitControls,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useControls } from "leva";
+import Loader from "./Loader";
+
+const CameraManager = () => {
+  return (
+    <CameraControls
+      maxAzimuthAngle={Math.PI / 2}
+      minAzimuthAngle={-Math.PI / 2}
+      maxPolarAngle={(2 * Math.PI) / 3}
+      minPolarAngle={Math.PI / 3}
+    />
+  );
+};
 
 const Scene = () => {
   return (
     <Canvas
+      fallback={<Loader />}
       camera={{
-        position: [3, 3, 3],
+        position: [0, 0, 1],
       }}
     >
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <group>
+        <CameraManager />
+        <Environment preset="sunset" />
+        <ambientLight intensity={0.6} />
+        <Gltf
+          src="/models/classroom.glb"
+          position={[110, -140, 40]}
+          rotation={[0, Math.PI, 0]}
+        />
+      </group>
     </Canvas>
   );
 };
